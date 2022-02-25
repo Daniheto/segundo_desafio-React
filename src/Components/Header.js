c8bd34d673d0897be8c18b2abf2ffd7a0829239b
Daniel Hernandez <daedheto@gmail.com>
2022-02-25 01:47:32 -0300

    Add files via upload
    segundo desafio

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Hammers Ultimate Club</h1>
+      <NavBar />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Main.js
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <main>Contenido Principal</main>;
+};
+
+export default Main;
