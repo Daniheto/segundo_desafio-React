9e42b5c41d65678b2c4646f520e5ae6a05bc8471
Daniel Hernandez Torres <daedheto@gmail.com>
2022-02-25 01:19:03 -0300

    desafio2

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <a href="https://www.google.com/">Google</a>
+      <a href="https://plataforma.coderhouse.com/">Coder</a>
+      <a href="https://www.youtube.com/">You Tube</a>
+    </nav>
+  );
+};
+
+export default NavBar;
