963351ae02c99535ca31c717000fe152f652d1c4
Daniel Hernandez Torres <daedheto@gmail.com>
2022-03-02 18:11:43 -0300

    segundo_desafio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Header />
-      <ItemListConatainer nombre="Daniel" apellido="Hernandez" />
+      <ItemListConatainer nombre="Daniel" apellido="Hernández" />
       <Footer />
     </>
   );
